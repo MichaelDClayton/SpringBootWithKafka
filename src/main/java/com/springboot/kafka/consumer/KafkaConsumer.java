b6d4baf3cda77e402665b27bffffe9c4cc198594
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-08-27 15:44:43 -0400

    Java Consumer added. Success.

---
+++ src/main/java/com/springboot/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.springboot.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "demotopic", groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info("Message received: {}", message);
+    }
+}
